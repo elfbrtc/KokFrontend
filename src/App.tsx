a495b52086f9923bfc52ba537d53f7d93f6d3c98
elfbrtc <elif.barutcu.5353@hotmail.com>
2023-10-11 21:40:18 +0300

    -connected service with fetch -added proxy -creacted model

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Example from './components/Example'
+import { Note } from './models/note';
 
 function App() {
+
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(()=>{
+
+    async function loadNote(){
+      try {
+        const response = await fetch("/api/notes", {method: "GET"});
+        const notes = await response.json();
+        setNotes(notes);
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+      
+    }
+    loadNote();
+
+  },[]);
+
   return (
     <>
     <Example></Example>
+    {JSON.stringify(notes)}
     </>
   );
 }
